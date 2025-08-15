51ec79b8885bc8a9f2203a963845718a3b9537ec
1Menna <meno903010@gmail.com>
2025-08-15 20:31:39 +0300

    last done ver1

---
+++ src/appliances/appliances.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-appliances',
+  imports: [],
+  templateUrl: './appliances.html',
+  styleUrl: './appliances.css'
+})
+export class Appliances {
+
+}
