3abb543ca22e12eb597226e5ecbec41a321acc29
1Menna <meno903010@gmail.com>
2025-08-15 20:37:33 +0300

    last done after add roterLink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,23 @@
 import { Routes } from '@angular/router';
+import { Electronics } from './../electronics/electronics';
+import { Appliances } from '../appliances/appliances';
+import { Beauty } from '../beauty/beauty';
+import { Games } from '../games/games';
+import { Kids } from '../kids/kids';
+import { Men } from '../men/men';
+import { Women } from '../women/women';
+import { Contact } from '../contact/contact';
+import { Home } from '../home/home';
 
 export const routes: Routes = [
-  
+  { path: "electronics", component: Electronics, title: "electronics" },
+  { path: "appliances", component: Appliances, title: "appliances" },
+  { path: "beauty", component: Beauty, title: "beauty" },
+  { path: "games", component: Games, title: "games" },
+  { path: "kids", component: Kids, title: "kids" },
+  { path: "men", component: Men, title: "men" },
+  { path: "women", component: Women, title: "women" },
+  { path: "contact", component: Contact, title: "contact" },
+  { path: "home", component: Home, title: "home" },
+  { path: "", redirectTo: "home", pathMatch: "full" }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "../navbar/navbar";
+import { Men } from '../men/men';
+import { Women } from '../women/women';
+import { Kids } from '../kids/kids';
+import { Beauty } from '../beauty/beauty';
+import { Games } from '../games/games';
+import { Appliances } from '../appliances/appliances';
+import { Electronics } from '../electronics/electronics';
+import { Home } from '../home/home';
+import { Contact } from '../contact/contact';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar,Men,Women,Kids,Beauty,Games,Appliances,Electronics,Home,Contact],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
