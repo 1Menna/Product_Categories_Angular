eb8e41acddc1a93a67d04ead8cca8d09ab0c33ca
1Menna <meno903010@gmail.com>
2025-08-16 00:55:35 +0300

    _

--- src/games/games.ts
+++ src/games/games.ts
@@ -9,8 +9,8 @@ import { Iproduct } from '../iproduct';
 })
 export class Games {
   products:Iproduct[]=[
-      {id:1,title:"Nilco Twist And Turn Cylinder Pack",price:500,category:"games",img:"/games/Nilco Twist And Turn Cylinder Pack"},
-      {id:2,title:"Mattel Playing Card Game MAT41001M",price:65,category:"games",img:"/games/Mattel Playing Card Game MAT41001M"},
+      {id:1,title:"Nilco Twist And Turn Cylinder Pack",price:500,category:"games",img:"/games/Nilco Twist And Turn Cylinder Pack.png"},
+      {id:2,title:"Mattel Playing Card Game MAT41001M",price:65,category:"games",img:"/games/Mattel Playing Card Game MAT41001M.png"},
       {id:3,title:"Tooth Extraction Crocodile Toy Parent Child Game Whole Person Toy",price:94,category:"games",img:"/games/Tooth Extraction Crocodile Toy Parent Child Game Whole Person Toy.png"},
       {id:4,title:"Rastar - Oracle Red Bull Racing RB18 118 With Remote Control - Black - 94800",price:2200,category:"games",img:"/games/Rastar - Oracle Red Bull Racing RB18 118 With Remote Control - Black - 94800.png"},
       {id:5,title:"Magnetic Sticks Balls and Rods Set, Building Blocks and Sticks Educational",price:784,category:"games",img:"/games/Magnetic Sticks Balls and Rods Set, Building Blocks and Sticks Educational.png"},
